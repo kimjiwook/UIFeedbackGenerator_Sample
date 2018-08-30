079263c6971af46131cd350452319efc925aaefe
jiwook <xodhks_0113@naver.com>
2018-08-30 16:12:04 +0900

    FeedbackGenerator Sample.

--- UIFeedbackGeneratorSample/ViewController.swift
+++ UIFeedbackGeneratorSample/ViewController.swift
@@ -19,7 +19,42 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    /// UIButton Touch Up Inside Action
+    ///
+    /// - Parameter sender: UIButton (tag)
+    @IBAction func doFeedbackGenerator(_ sender: Any) {
+        let senderButton:UIButton = sender as! UIButton
+        
+        switch senderButton.tag {
+        case 1:
+            let generator = UINotificationFeedbackGenerator()
+            generator.notificationOccurred(.success)
+            
+        case 2:
+            let generator = UINotificationFeedbackGenerator()
+            generator.notificationOccurred(.warning)
+            
+        case 3:
+            let generator = UINotificationFeedbackGenerator()
+            generator.notificationOccurred(.error)
 
-
+        case 4:
+            let generator = UIImpactFeedbackGenerator(style: .light)
+            generator.impactOccurred()
+            
+        case 5:
+            let generator = UIImpactFeedbackGenerator(style: .medium)
+            generator.impactOccurred()
+            
+        case 6:
+            let generator = UIImpactFeedbackGenerator(style: .heavy)
+            generator.impactOccurred()
+        
+        default:
+            let generator = UISelectionFeedbackGenerator()
+            generator.selectionChanged()
+        }
+    }
 }
 
